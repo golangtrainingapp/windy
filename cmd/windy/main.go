846ead640e1a4b56e432df13a36e5426243563a0
anandkumar seshadri <anandkumar.seshadri@unisys.com>
2025-07-24 15:40:01 -0500

    Added print statements.

--- cmd/windy/main.go
+++ cmd/windy/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/fatih/color"
 	"github.com/golangtrainingapp/windy"
 	"os"
 	"path/filepath"
@@ -16,8 +17,9 @@ func main() {
 		_ = os.Setenv("WINDY_API_KEY", "mxJW8fEadecqILVj7RWBdhUfJ38Ou0Bv")
 	}
 	apiKey = os.Getenv("WINDY_API_KEY")
-
-	resp, err := windy.GetWeather(53.1900, -112.2500, apiKey)
+	latitude := 53.1900
+	longitude := -112.2500
+	resp, err := windy.GetWeather(latitude, longitude, apiKey)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -30,7 +32,8 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+	PrintToConsole(resp, latitude, longitude)
+	//fmt.Println(resp)
 }
 
 func WriteToFile(path string, data []byte) error {
@@ -44,5 +47,23 @@ func WriteToFile(path string, data []byte) error {
 	}()
 	_, _ = f.Write(data)
 	return nil
+
+}
+
+func PrintToConsole(response windy.Windy_Realtime_Report, latitude, longitude float64) {
+	color.Cyan("Latitude: %v, Longitude: %v", latitude, longitude)
+	color.Blue("-------------------------------------------\n")
+	color.Cyan("A sample partial data is displayed to the console:\n")
+	color.Green("Surface Temperature:  %v", response.TempSurface[0])
+	color.Green("Surface Dewpoint: %v", response.DewpointSurface[0])
+	color.Green("Surface Wind: %v", response.WindUSurface[0])
+	color.Green("Surface Gust: %v", response.GustSurface[0])
+	color.Green("Surface Cape: %v", response.CapeSurface[0])
+	color.Green("Surface Pressure: %v", response.PressureSurface[0])
+	color.Green("Surface Ptype: %v", response.PtypeSurface[0])
+	color.Green("Surface Lcloud: %v", response.LcloudsSurface[0])
+	color.Green("Surface Hcloud %v", response.HcloudsSurface[0])
+	color.Green("Surface Mcloud %v", response.McloudsSurface[0])
+	color.Green("Surface Precipitation %v", response.Past3HprecipSurface[0])
 
 }
