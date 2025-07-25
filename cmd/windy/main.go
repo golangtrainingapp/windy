9f4f0d057a4e9a0fcc35dd755eda8a6c279f3493
anandkumar seshadri <anandkumar.seshadri@unisys.com>
2025-07-25 13:21:45 -0500

    Modification to Print Statements

--- cmd/windy/main.go
+++ cmd/windy/main.go
@@ -51,15 +51,20 @@ func WriteToFile(path string, data []byte) error {
 }
 
 func PrintToConsole(response windy.Windy_Realtime_Report, latitude, longitude float64) {
+	// Use your own io.Writer output
+	c := color.New(color.FgCyan).Add(color.Underline)
+
+	fmt.Println()
 	color.Cyan("Latitude: %v, Longitude: %v", latitude, longitude)
 	color.Blue("-------------------------------------------\n")
 	color.Cyan("A sample partial data is displayed to the console:\n")
-	color.Green("Air Temperature:  %v", fmt.Sprintf("%.2f", response.TempSurface[0]))
-	color.Green("Dewpoint: %v", fmt.Sprintf("%.2f", response.DewpointSurface[0]))
-	color.Green("Wind: %v", fmt.Sprintf("%.2f", response.WindUSurface[0]))
-	color.Green("Wind Gust: %v", fmt.Sprintf("%.2f", response.GustSurface[0]))
-	color.Green("Cape: %v", fmt.Sprintf("%.2f", response.CapeSurface[0]))
-	color.Green("Air Pressure: %v", fmt.Sprintf("%.2f", response.PressureSurface[0]))
+
+	_, _ = c.Print("Air Temperature: ", fmt.Sprintf("%.2f", response.TempSurface[0]), " C,")
+	_, _ = c.Print(" Dewpoint: ", fmt.Sprintf("%.2f", response.DewpointSurface[0]), " C,")
+	_, _ = c.Print(" Wind: ", fmt.Sprintf("%.2f", response.WindUSurface[0]), " C,")
+	_, _ = c.Print(" Wind Gust: ", fmt.Sprintf("%.2f", response.GustSurface[0]), " mph,")
+	_, _ = c.Print(" Cape: ", fmt.Sprintf("%.2f", response.CapeSurface[0]), " J/Kg,")
+	_, _ = c.Print(" Air Pressure: ", fmt.Sprintf("%.2f", response.PressureSurface[0]), " mb,")
 	iprecip := response.PtypeSurface[0]
 	var precipitation string
 	switch iprecip {
@@ -74,15 +79,17 @@ func PrintToConsole(response windy.Windy_Realtime_Report, latitude, longitude fl
 	case 7:
 		precipitation = "Mixture of Rain and snow"
 	case 8:
-		precipitation = "Ice pellets"
+		precipitation = "Ice Pellets"
 	default:
-		precipitation = "No precipitation"
+		precipitation = "No Precipitation"
 	}
-	color.Green("Precipitation: %v", precipitation)
-	color.Green("Relative humidity: %v", fmt.Sprintf("%.2f", response.RhSurface[0]))
-	color.Green("Low Cloud: %v", fmt.Sprintf("%.2f", response.LcloudsSurface[0]))
-	color.Green("High Cloud %v", fmt.Sprintf("%.2f", response.HcloudsSurface[0]))
-	color.Green("Medium Cloud %v", fmt.Sprintf("%.2f", response.McloudsSurface[0]))
-	color.Green("Overall snow for the preciding 3 hours %v", fmt.Sprintf("%.2f", response.Past3HprecipSurface[0]))
+
+	_, _ = c.Print(" Precipitation: ", precipitation, " ,")
+
+	_, _ = c.Print(" Relative Humidity: ", fmt.Sprintf("%.2f", response.RhSurface[0]), " %,")
+	_, _ = c.Print(" Low Cloud: ", fmt.Sprintf("%.2f", response.LcloudsSurface[0]), " ,")
+	_, _ = c.Print(" High Cloud: ", fmt.Sprintf("%.2f", response.HcloudsSurface[0]), " ,")
+	_, _ = c.Print(" Medium Cloud: ", fmt.Sprintf("%.2f", response.McloudsSurface[0]), " ,")
+	_, _ = c.Print(" Overall snow for the preciding 3 hours: ", fmt.Sprintf("%.2f", response.Past3HprecipSurface[0]), " mm,")
 
 }
